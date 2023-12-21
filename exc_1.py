9450c5cfb4969dd1f570f8efa883941dd2596a59
wojtek239 <wojtekopelt1@wp.pl>
2023-12-21 14:16:09 +0100

    flask exc 1

---
+++ exc_1.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sekretny_klucz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    date_registered = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+db.create_all()
+
+
+@app.route('/')
+def home():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        days_registered = (datetime.utcnow() - user.date_registered).days
+        return f'Hello, {user.username}. You’re with us for {days_registered} days.'
+    return 'You are not logged in.'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Account created successfully. You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            session['user_id'] = user.id
+            flash('Login successful.', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your username and password.', 'danger')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
